c7a60575baf78582fcef19a9cb2d8608e074d9a4
Jimwell <jimwell.h@gmail.com>
2022-06-09 14:47:18 +0800

    change code: print "Github test 02"

--- src/test.java
+++ src/test.java
@@ -1,5 +1,6 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("Github test 01");
+        //System.out.println("Github test 01");
+        System.out.println("Github test 02");
     }
 }
